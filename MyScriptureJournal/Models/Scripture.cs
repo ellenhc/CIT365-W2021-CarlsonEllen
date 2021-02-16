63cfa64a4152d45b29466dc6641f4bab344a966c
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-16 13:46:01 -0800

    Created my scripture journal project

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; } //The database's primary key
+        public string Title { get; set; }
+        public string ScriptureText { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Note { get; set; }
+    }
+}
