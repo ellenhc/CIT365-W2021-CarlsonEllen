9e2ba6aa09c14b9eb5f104dfac4291bd30f12c68
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-19 23:37:09 -0800

    Can now sort by keyword from the note field

--- MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyScriptureJournal.Data;
 using MyScriptureJournal.Models;
@@ -20,10 +21,21 @@ namespace MyScriptureJournal.Pages.Scriptures
         }
 
         public IList<Scripture> Scripture { get;set; }
-        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Books { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ScriptureBook { get; set; }
+
         public async Task OnGetAsync()
         {
-            Scripture = await _context.Scripture.ToListAsync();
+            var scriptures = from x in _context.Scripture
+                             select x;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Note.Contains(SearchString));
+            }
+            Scripture = await scriptures.ToListAsync();
         }
     }
 }
