378100ae4b47bfdf6bb4deed6ce9ca33f5239715
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-20 10:21:53 -0800

    SeedData class added. CRUD page directive changed

---
+++ MyScriptureJournal/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using MyScriptureJournal.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyScriptureJournal.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MyScriptureJournalContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MyScriptureJournalContext>>()))
+            {
+                // Look for any movies.
+                if (context.Scripture.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Scripture.AddRange(
+                    new Scripture
+                    {
+                        Book = "Moses",
+                        Chapter = "1",
+                        Verse = "39",
+                        DateAdded = DateTime.Parse("2021-2-20"),
+                        Note = "God wants us to return to Him."
+                    },
+
+                    new Scripture
+                    {
+                        Book = "D&C",
+                        Chapter = "138",
+                        Verse = "42",
+                        DateAdded = DateTime.Parse("2021-2-20"),
+                        Note = "After His Resurrection..."
+                    },
+
+                    new Scripture
+                    {
+                        Book = "Alma",
+                        Chapter = "7",
+                        Verse = "12",
+                        DateAdded = DateTime.Parse("2021-2-20"),
+                        Note = "The Savior suffered what we suffer so He can lift us up."
+                    },
+
+                    new Scripture
+                    {
+                        Book = "Matthew",
+                        Chapter = "11",
+                        Verse = "28",
+                        DateAdded = DateTime.Parse("2021-2-20"),
+                        Note = "He will take our burdens."
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
