68610c4133295b3ba37a81d097a807fd5591c4c4
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-20 10:04:00 -0800

    Created a new MyScriptureJournal since I changed the db

--- MyScriptureJournal/Models/Scripture.cs
+++ MyScriptureJournal/Models/Scripture.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyScriptureJournal.Models
 {
     public class Scripture
     {
         public int ID { get; set; } //The database's primary key
+        public string Book { get; set; }
+        public string Chapter { get; set; }
+        public string Verse { get; set; }
+
+        [Display(Name = "Scripture Reference")]
+        public string Reference
+        {
+            get
+            {
+                return Book + " " + Chapter + ":" + Verse;
+            }
+        }
+
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+        public string Note { get; set; }
     }
 }
