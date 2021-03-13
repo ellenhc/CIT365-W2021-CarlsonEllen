318f31c2217c34dc7c8f82f82aa38085d2423bfe
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-03-12 22:25:33 -0800

    Added Person class

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -5,34 +5,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Display(Name = "Last Name")]
-        [Required]
-        [StringLength(50, MinimumLength = 2)]
-        public string LastName { get; set; }
-
-        [Display(Name = "First Name")]
-        [Required]
-        [StringLength(50, MinimumLength = 2)]
-        [Column("FirstName")]
-        public string FirstMidName { get; set; }
-
         [Display(Name = "Enrollment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
